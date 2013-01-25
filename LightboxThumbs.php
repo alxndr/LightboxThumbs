e352006187c3f9daa35f7cf35898ac1437874b48
Alexander <alxndr+github@gmail.com>
2013-01-24 18:19:39 -0800

    v0.1.4.1 copied from MediaWiki http://www.mediawiki.org/w/index.php?title=Extension:LightboxThumbs/LightboxThumbs-0.1.4.1.php&oldid=345894

--- LightboxThumbs.php
+++ LightboxThumbs.php
@@ -2,18 +2,23 @@
 /*
  * LightboxThumbs extension
  * by Alexander, http://www.mediawiki.org/wiki/User:Alxndr
- * Displays thumbnailed images full-size in window using Lokesh Dhakar's Lightbox 2 (http://www.lokeshdhakar.com/projects/lightbox2/).
- * 
- * Licensed under Creative Commons Attribution-NonCommercial license 3.0: http://creativecommons.org/licenses/by-nc/3.0/
- * 
- 
+ * Changes/Bug Fixes made by Kinsey Moore and Rgoodermote
+ * Changes/Compatibility for MW 1.16 by Michael Markert
+ *
+ * Displays thumbnailed images full-size in window
+ * using Lokesh Dhakar's Lightbox 2 (http://www.lokeshdhakar.com/projects/lightbox2/)
+ *
+ * Licensed under Creative Commons Attribution-NonCommercial license 3.0:
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * Requirements: MW 1.16 (older versions of this extension still available)
+ *               Lightbox 2
+ *
  * Bugs: will make very large images take over your screen! doesn't do any resizing
  *       if there are multiple galleries on one page, they are treated as being part of one big slideshow
  *       will probably break on images with a / or ? in the name
  *       probably eats a lot of resources if you have a ton of thumbnails in a page...
- *       doesn't work with SVGs (?)
- *       doesn't work on MW before 1.9 (?)
- * 
+ *
  * Todo: make galleries register as separate slideshows
  *       only add hook if there's a thumbnail in the page
  *       change thumbnail caption source to preserve markup, like galleries
@@ -24,89 +29,107 @@ if ( !defined( 'MEDIAWIKI' ) )
     die( 'This is a MediaWiki extension, and must be run from within MediaWiki.' );
 
 $wgExtensionCredits['other'][] = array(
-    'name'        => 'LightboxThumbs',
-    'url'         => 'http://www.mediawiki.org/wiki/Extension:LightboxThumbs',
-    'author'      => '[http://www.mediawiki.org/wiki/User:Alxndr Alexander], alxndr<span style="position:absolute;display:none"> no spam, please </span>@gmail.com',
-    'description' => 'Displays full-size images with [http://www.lokeshdhakar.com/projects/lightbox2/ Lightbox 2] when clicking on thumbnails.',
-    'version'     => '0.1.3-alpha4'
+  'name'        => 'LightboxThumbs',
+  'url'         => 'http://www.mediawiki.org/wiki/Extension:LightboxThumbs',
+  'author'      => '[http://www.mediawiki.org/wiki/User:Alxndr Alexander], (alxndr+mediawiki@gmail.com) and others',
+  'description' => 'Displays full-size images with [http://www.lokeshdhakar.com/projects/lightbox2/ Lightbox 2] when clicking on thumbnails.',
+  'version'     => '0.1.4.1'
 );
 
-if ($lightboxThumbsFilesDir) ## don't add the hook unless we have a directory to look at
-{
-    $lightboxThumbsFilesDir = rtrim(trim($lightboxThumbsFilesDir),'/'); # strip whitespace, then any trailing /
-    ## we'd rather subclass Linker and change how it builds thumbnails and galleries, but we can't, so modifying the HTML once it's built will have to suffice
-    $wgHooks['BeforePageDisplay'][] = 'efBeforePageDisplay';
+
+if ($lightboxThumbsFilesDir) { ## don't add the hook unless we have a directory to look at
+  $lightboxThumbsFilesDir = rtrim(trim($lightboxThumbsFilesDir),'/'); # strip whitespace, then any trailing /
+  ## we'd rather subclass Linker and change how it builds thumbnails and galleries, but we can't, so modifying the HTML once it's built will have to suffice
+  $wgHooks['BeforePageDisplay'][] = 'efBeforePageDisplay';
 }
 
-function efDebugVar($varName,$var)
-{
-    return "\n\n<!--\n$varName: ".str_replace('--','__',print_r($var,true))."\n-->\n\n";
+function efDebugVar($varName,$var) {
+  return "\n\n<!--\n$varName: ".str_replace('--','__',print_r($var,true))."\n-->\n\n";
 }
 
-## TODO merge two efRewrite fxns into one, and send it normalized data from callback fxns which will interpret the $matches from each preg_replace?
-## ...that'd make it easier to group galleries into lightbox slideshows
-function efRewriteThumbImage($matches)
-{
-    ## see comments in first $pattern in efBeforePageDisplay() for what the pieces of $matches are
-    global $wgOut, $lightboxThumbsDebug;
-    if ($lightboxThumbsDebug) { global $wgContLang; }
-    $titleObj = Title::newFromText(rawurldecode($matches[2]));
-    $image = wfFindFile($titleObj,false,false,true); ## wfFindFile($titleObj,false,false,true) to bypass cache
-    return $matches[1].' href="'.$image->getURL().'" class="image" rel="lightbox" title="'
-        .htmlspecialchars( $wgOut->parse("'''[[:".$titleObj->getFullText()."|".$titleObj->getText()."]]:''' ").$matches[3] )
-        .'" '.$matches[4].$matches[5]
-        .($lightboxThumbsDebug?efDebugVar('$matches',$matches).efDebugVar('$titleObj',$titleObj).efDebugVar('$image',$image).efDebugVar('$wgContLang->namespaceNames',$wgContLang->namespaceNames):'');
+
+# debug
+#$lightboxThumbsDebug = true;
+# .($lightboxThumbsDebug?efDebugVar('$matches',$matches).efDebugVar('$titleObj',$titleObj).efDebugVar('$image',$image):'');
+# .($lightboxThumbsDebug?efDebugVar('$matches',$matches).efDebugVar('$titleObj',$titleObj).efDebugVar('$image',$image).efDebugVar('$wgContLang->namespaceNames',$wgContLang->namespaceNames):'');
+
+
+function efRewriteThumbImage($matches) {
+  // see comments in first $pattern in efBeforePageDisplay() for what the pieces of $matches are
+  global $wgOut, $lightboxThumbsDebug;
+  if ($lightboxThumbsDebug) { global $wgContLang; }
+
+  // get the image object ($3 for gallery and $7 for single images)
+  $titleObj = Title::newFromText(rawurldecode($matches[3]?$matches[3]:$matches[7]));
+  $image = wfFindFile($titleObj,false,false,true); ## wfFindFile($titleObj,false,false,true) to bypass cache
+
+  if($matches[1]) {
+    // is gallery:
+    return $matches[2].' href="'.$image->getURL().'" class="image" rel="lightbox[gallery]" title="'
+      .strip_tags($matches[5])        // strip tags for the title! (eg.for Special:NewFiles)
+      .'" '.$matches[4].$matches[5]."</div>"
+      .($lightboxThumbsDebug?efDebugVar('$matches',$matches):'');
+    } else {
+      // is single image:
+      $caption = ((!$matches[13] || $matches[13]=="")?$matches[15]:$matches[13]);
+      return $matches[6].' href="'.$image->getURL().'" class="image" rel="lightbox" title="'
+        .strip_tags($caption)
+        .'" '.$matches[10].$matches[11].$matches[12].$matches[13].$matches[14]
+        .($lightboxThumbsDebug?efDebugVar('$matches',$matches):'');
+    }
 }
 
-function efRewriteGalleryImage($matches)
-{
-    ## see comments in second $pattern in efBeforePageDisplay() for what the pieces of $matches are
-    global $wgOut, $lightboxThumbsDebug;
-    $titleObj = Title::newFromText(rawurldecode($matches[2]));
-    $image = wfFindFile($titleObj,false,false,true);
-    return $matches[1].' href="'.$image->getURL().'" class="image" rel="lightbox[gallery]" title="'
-        .htmlspecialchars( $wgOut->parse("'''[[:File:".$titleObj->getFullText()."|".$titleObj->getText()."]]:''' ").$matches[4] )
-        .'" '.$matches[3].$matches[4]."</div>"
-        .($lightboxThumbsDebug?efDebugVar('$matches',$matches).efDebugVar('$titleObj',$titleObj).efDebugVar('$image',$image):'');
+
+
+function efBeforePageDisplay($out) {
+  global $lightboxThumbsFilesDir, $wgContLang;
+
+  $out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/prototype.js"></script>');
+  $out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/scriptaculous.js?load=effects,builder"></script>');
+  #$out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/scriptaculous.js?load=effects,builder,dragdrop"></script>');
+  $out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/builder.js"></script>');
+
+  #$out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/lightbox%20(uncompressed).js"></script>');
+  $out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/lightbox.js"></script>');
+  $out->addScript('<link rel="stylesheet" href="'.$lightboxThumbsFilesDir.'/css/lightbox.css" type="text/css" media="screen" />'); # addStyle() is a pain
+
+  ## ideally we'd do this with XPath, but we'd need valid XML for that, so we'll do it with some ugly regexes
+  ## (could use a regex to pull out all div.thumb, maybe they're valid XML? ...probably not)
+
+  ## regex for thumbnails for > MW 1.16
+  ## <a href="/wiki/File:Test.jpg" class="image"><img alt="" src="/wiki/images/thumb/Test.jpg/120px-Test.jpg" width="120" height="120"></a>
+  $pattern = '/
+    (?(?=<div\s*class="gallerybox)                                  ## IF GALLERY THEN
+      <div\s*class="(gallery).+?                                      #$1: gallery
+      (<a[^>]+?)                                                      #$2: opening a tag
+      \s*href="[^"]*(?:'.urlencode($wgContLang->namespaceNames[6]).')
+      ([^"\/]+)                                                       #$3: Image Name
+      "\s*class="image"\s*
+      ([^>]*>.+?<div\s*class="gallerytext">)                          #$4: end of open a through start of caption
+      \s*(?:<p>\s*)?
+      (.+?)\n?<[^<>]+>                                                #$5: caption is raw HTML (wrap in php)
+      (?:\s*<\/p>|<br\s*\/?>)?\s*<\/div>\s*<\/div>
+    |                                                       ## ELSE
+      #$1 to $5 unmatched (see above)
+      (<a[^>]+?)                                                      #$6: opening a tag
+      \s*href="[^"]*(?:'.urlencode($wgContLang->namespaceNames[6]).')
+      ([^"\/]+)                                                       #$7: imageName
+      "\s*class="image"\s*
+      (title="([^"]+)")?                                              #$8 & $9: optional title ($8:HTML, $9:TXT)
+      ([^>]*>)                                                        #$10: remainder of opening a tag
+      \s*
+      (<img[^"])                                                      #$11: the img start tag
+      (alt=")                                                         #$12: the img alt tag
+      ([^"]{0,400})                                                   #$13: the img alt tag = Title
+      ([^>]*>)                                                        #$14: the img end
+      (?=.*thumbcaption.+?(?:\/div>)(.+?)(?:<\/div>)|)                #$15: Thumbcaption (preceeds 12 if not empty)
+    )                                                       ## ENDIF
+  /sx';
+
+  $thumbnailsDone = preg_replace_callback($pattern, 'efRewriteThumbImage', $out->getHTML());
+  $out->clearHTML();
+  $out->addHTML($thumbnailsDone);
+
+  return true;
 }
 
-function efBeforePageDisplay($out)
-{
-    global $lightboxThumbsFilesDir, $wgContLang;
-    $out->addScript('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/prototype/1.6.0.3/prototype.js"></script>');
-    $out->addScript('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.2/scriptaculous.js?load=effects,builder"></script>');
-    $out->addScript('<script type="text/javascript" src="'.$lightboxThumbsFilesDir.'/js/lightbox.js"></script>');
-    $out->addScript('<link rel="stylesheet" href="'.$lightboxThumbsFilesDir.'/css/lightbox.css" type="text/css" media="screen" />'); # addStyle() is a pain
-    
-    ## ideally we'd do this with XPath, but we'd need valid XML for that, so we'll do it with some ugly regexes
-    ## (could use a regex to pull out all div.thumb, maybe they're valid XML? ...probably not)
-    
-    ## regex for thumbnails
-    $pattern = '/(<a[^>]+?)                           # $1: start of opening <a> tag through start of href attribute in <a> tag, so we can keep it intact
-                 \s*href="[^"]*(?:'.$wgContLang->namespaceNames[6].'): # dont care about start of original link href...
-                 ([^"\/]+)                            # $2: ...but end is wiki name for the image
-                 "\s*class="image"\s*title="          #
-                 ([^"]+)                              # $3: link title becomes image caption
-                 "\s*                                 #
-                 ([^>]*>)                             # $4: remainder of opening <a> tag
-                 \s*                                  #
-                 (<img[^>]+?class="thumbimage"[^>]*>) # $5: the img tag itself
-                /x';
-    $thumbnailsDone = preg_replace_callback($pattern, 'efRewriteThumbImage', $out->getHTML());
-    
-    ## regex for galleries
-    $pattern = '/(<div\s*class="gallerybox".+?div\s*class="thumb".+?) # $1: div.gallerybox opening tag through href attribute, so we can keep it intact
-                 \s*href="[^"]+"\s*class="image"\s*                   # this is getting replaced
-                 title="([^"]+)"                                      # $2: link title attribute holds wiki name for the image
-                 ([^>]*>.+?<div\s*class="gallerytext">)               # $3: end of open <a> through start of caption
-                 \s*(?:<p>\s*)?                                       #
-                 (.+?)                                                # $4: caption is raw HTML... (may choke if contains an ending div)
-                 (?:\s*(<\/p>|<br\s*\/?>))?\s*<\/div>                 #
-                /sx';
-    $allDone = preg_replace_callback($pattern, 'efRewriteGalleryImage', $thumbnailsDone);
-    $out->clearHTML();
-    $out->addHTML($allDone);
-    
-    return true;
-}
-?>
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-LightboxThumbs
-==============
+# LightboxThumbs
 
-A MediaWiki plugin to show thumbnails in lightboxes.
+LightboxThumbs incorporates Lokesh Dhakar's fullsize image viewer [Lightbox 2](http://www.huddletogether.com/projects/lightbox2/) into [MediaWiki](http://www.mediawiki.org/wiki/MediaWiki)'s thumbnails and image galleries.
+
+When a user clicks on a thumbnailed image or an image in a gallery, the page darkens and the image is displayed full-size "on top of" the page. A caption below the image includes the image's title and a link to its description page, along with the caption on the thumbnail, if any. Clicking anywhere off the image returns everything to normal.
+
+Images in galleries are treated as being part of a slideshow, and have "previous" and "next" images.
+
+
+## Installation for MW >= 1.16 \*
+
+0.  `cd extensions/ && git clone https://github.com/alxndr/LightboxThumbs.git`
+1.  Download Lokesh Dhakar's [Lightbox 2](http://lokeshdhakar.com/projects/lightbox2/releases/), rename the directory to "lightbox", and put it in your `extensions/` directory. JS, CSS, and image files need to be accessible in a browser.
+2.  Edit `extensions/lightbox.js`: change `fileLoadingImage` and `fileBottomNavCloseImage` so they're indicating where the images actually are.
+3.  Edit `LocalSettings.php` to add:
+
+        $lightboxThumbsFilesDir = '/extensions/lightbox';
+        include_once("$IP/extensions/LightboxThumbs.php");
+
+\* If for some reason you're running MW 1.10 or earlier, try using v0.1.2. If you're between 1.11 and 1.15, try v0.1.3.
+
+## Bugs
+
+* Very large images may take over your screen! That is, it doesn't do any resizing.
+* If there are multiple galleries on one page, they are treated as being part of one big slideshow.
+* May not work with images that have slashes in the name.
+
+
+## Caveat ūsor
+
+This was originally developed on an installation of 1.11 with an old custom skin and a bunch of extensions and a lot of general hackery. Unforeseen behavior is a very real possibility.
+
+That said, let me know and I'll try to help fix anything that goes wrong.
+
+
+## Props
+
+Thanks to Piotr, Timmy, and Georges for helping me debug and test new versions of v0.1.3.
+
+Thanks to Kinsey Moore, [Rgoodermote](http://www.mediawiki.org/wiki/User:Rgoodermote), [Michael Markert](http://www.mediawiki.org/wiki/User:Auco), and [Aczs](http://www.mediawiki.org/wiki/Special:Contributions/Aczs) for taking the ball and running to create v0.1.4, with bug fixes and tweaks to get it working in MW 1.16.
+
